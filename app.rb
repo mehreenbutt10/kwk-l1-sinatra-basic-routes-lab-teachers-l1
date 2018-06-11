812f65b455f40ce6c0aa6e5efe56379281f53fb7
Mehreen Butt <mbutt@republiccharterschools.org>
2018-06-11 19:23:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Mehreen"
+  end
+  
+  get '/hometown' do
+    "My hometown is St. Louis."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Girls Like You."
+  end
+  
 end
